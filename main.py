240a53f3feb1db3a67268cb634818b157685a367
PSirgun <Sirgun28@gmail.com>
2024-01-27 12:38:32 +0200

    1

---
+++ main.py
@@ -0,0 +1,23 @@
+import requests
+from bs4 import BeautifulSoup
+from sqlalchemy.engine import create_engine
+from sqlalchemy.orm import sessionmaker
+
+from models import Author, Quote
+import connect
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    ...
